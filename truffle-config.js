3e715509d44ea2a078b04665d24f2751b7ce60a5
Ashlesh Sortee <sorteeashlesh@gmail.com>
2021-03-02 00:04:47 +0530

    Update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,33 +1,14 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
+      host: "ganache",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "2020",    // Any network (default: none)
     },
   },
 
-  compilers: {
-    solc: {
-      version: "^0.6.0",    // Fetch exact version from solc-bin (default: truffle's version)
-      docker: false,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
-        optimizer: {
-          enabled: true,
-          runs: 200
-        },
-        evmVersion: "petersburg"
-      }
-    }
-  }
-}
-module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "2020",    // Any network (default: none)
-    },
+  db: {
+    enabled: false
   },
 
   compilers: {
